ba28ee609567afb8a41e643e9b59a96293a0e2a3
Vanessa Aguirre Diaz <143459791+Vanessa-22310274ceti@users.noreply.github.com>
2025-03-18 19:05:50 -0600

    001_Vanessa Aguirre
    Practica 2 parte 2 y 3, probando otras formas de sumar los valores de cada pixel de dos imágenes y comparar el efecto de cada función sobre la imagen final.

--- PRACTICA_2_22310274/P2_Operaciones_Aritmeticas.py
+++ PRACTICA_2_22310274/P2_Operaciones_Aritmeticas.py
@@ -9,8 +9,17 @@ import numpy as np
 img1 = cv2.imread('3D-Matplotlib.png')  # Cargar imagen 1
 img2 = cv2.imread('mainsvmimage.png')   # Cargar imagen 2
 
-add = img1+img2                 # Sumar ambas imagenes pixel por pixel
-                                # Si la suma supera los 255 habrá un desbordamiento
-cv2.imshow('Suma',add)          # Mostrar la imagen que resulta de la suma
-cv2.waitKey(0)                  # Esperar a que el usuario presione una tecla para cerrar la ventana
-cv2.destroyAllWindows()         # Cerrar todas las ventanas de OpenCV.
+# Sumar ambas imagenes pixel por pixel
+#Pt.1:
+add = img1+img2                 # Si la suma supera los 255 habrá un desbordamiento
+#Pt.2:
+add_cv2 = cv2.add(img1,img2)    # Si la suma excede 255, el valor se limita a 255 (no hay desbordamiento).
+#Pt.3: 
+weighted = cv2.addWeighted(img1, 0.6, img2, 0.4, 0)     # Cada imagen tendrá un peso diferente
+
+cv2.imshow('Suma directa',add)                      # Mostrar la imagen que resulta de la suma directa con signo
+cv2.imshow('Suma con la funcion cv2.add',add_cv2)   # Mostrar la imagen que resulta de la suma con la funcion cv2.add
+cv2.imshow('weighted',weighted)                     # Mostrar la imagen con diferentes pesos
+
+cv2.waitKey(0)                                      # Esperar a que el usuario presione una tecla para cerrar la ventana
+cv2.destroyAllWindows()                             # Cerrar todas las ventanas de OpenCV.
