4f944533e0eddb235d59301568961b64f57ca085
Vanessa Aguirre Diaz <143459791+Vanessa-22310274ceti@users.noreply.github.com>
2025-04-19 14:46:14 -0600

    001_Vanessa_Aguirre
    Practica 4: Mostrar el histograma de una imagen. Para este comit se cambio la imagen por una de colores mas solidos y definidos para ver el cambio en el histograma

--- PRACTICA_3_HISTOGRAMA_Y_ECUALIZACION/P3_Histograma_Imagen.py
+++ PRACTICA_3_HISTOGRAMA_Y_ECUALIZACION/P3_Histograma_Imagen.py
@@ -4,8 +4,8 @@
 import cv2
 import matplotlib.pyplot as plt
 
-img1 = cv2.imread('v2.jpeg')    # Cargar imagen
-img2 = cv2.imread('v2.jpeg', cv2.IMREAD_GRAYSCALE)    # Convertir a escala de grises
+img1 = cv2.imread('tata.jpg')    # Cargar imagen
+img2 = cv2.imread('tata.jpg', cv2.IMREAD_GRAYSCALE)    # Convertir a escala de grises
 
 cv2.imshow('Imagen 1',img1)             # Mostrar la imagen
 cv2.imshow('Imagen 2',img2)             # Mostrar la imagen
