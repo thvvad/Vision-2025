5516d304d111936b901ee28edb4f15f5cc781861
Vanessa Aguirre Diaz <143459791+Vanessa-22310274ceti@users.noreply.github.com>
2025-06-14 02:58:51 -0600

    009_Vanessa Aguirre
    Cambiar de python 3.13 a 3.7 y volver a cargar la dataset

--- PROYECTO/03_Descargar otra dataset.py
+++
@@ -1,5 +0,0 @@
-# Instala la librería de datasets si no está
-import subprocess
-import sys
-
-subprocess.check_call([sys.executable, "-m", "pip", "install", "datasets", "huggingface_hub", "Pillow"])
--- PROYECTO/04_Descargar deepfashion.py
+++
@@ -1,4 +0,0 @@
-from datasets import load_dataset
-
-# Cargar la versión del dataset DeepFashion publicada por 'junyanz'
-dataset = load_dataset("junyanz/deepfashion")
